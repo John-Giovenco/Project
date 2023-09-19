ae5bf5d0e1b4ccee2946694536424c061871bf50
John-Giovenco <JohnGiovenco93@gmail.com>
2023-09-19 11:15:29 -0700

    finishing up controller auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
@@ -37,6 +38,9 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES*/
 app.post("/auth/register", upload.single("picture"), register);
+
+/* Routes */
+app.use("/auth/register", authRoutes)
 
 /* mongoose setup*/
 const PORT = process.env.PORT || 6001;
