73041d63164bf423d461cb933fd1e4015d968891
John-Giovenco <JohnGiovenco93@gmail.com>
2023-09-22 15:55:05 -0700

    working on my post widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,34 @@
 import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
+Import MyPostWidget from "scenes/widgets/MyPostWidget"
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:100px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap=".05rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath}></MyPostWidget>
+        </Box>
+        {isNonMobileScreens && <Box FexBasis="26%"></Box>}
+      </Box>
     </Box>
   );
 };
