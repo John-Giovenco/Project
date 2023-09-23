5037233a4b3927cd072ee4164e5b6c022d301926
John-Giovenco <JohnGiovenco93@gmail.com>
2023-09-23 08:49:17 -0700

    finished posts widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
-Import MyPostWidget from "scenes/widgets/MyPostWidget"
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:100px)");
